c59b4a5cde7be3766e1b8a1e025643a48fab4a5e
Hanan <122808899+Hanan71@users.noreply.github.com>
2026-09-05 21:01:54 +0300

    Add files via upload

---
+++ FullStack_files/Services/EmailSender.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Threading.Tasks;
+
+namespace CoolCompanyEstore.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // هنا فقط تكتب رسالة في الكونسول بدل الإرسال الحقيقي
+            Console.WriteLine($"Fake Email Sent to {email}: {subject}");
+            return Task.CompletedTask;
+        }
+    }
+}
